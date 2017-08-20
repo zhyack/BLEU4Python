00d591db260ec67e19a13a2c9556c510f19bee68
zhy <zhyack@126.com>
2017-08-20 09:28:59 +0800

    v2.0

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # BLEU4Python
-A light python version of moses BLEU.
+A light-weight python version of moses BLEU.
 
 ## Use Python Now
-Moses version of BLEU calculation is widely used in academic researches and accepted by scholarly community. As python seems to be the main force of deep learning development language, people may want to python version of this algorithm, which perform exactly the same way with the perl one.
+Moses version of BLEU calculation is widely used in academic researches and well accepted by scholarly community. As python seems to be the main force of deep learning development language, people may want the python version of this algorithm, which performs exactly the same way with the perl one.
 
-Here is what you are looking for. A python script to work the same as the moses' `multi-bleu.perl`. What's more, it can also be used as a module of python. Just `import bleu` and use `bleu.corpus_bleu` or `bleu.bleu_count` to do your work in detail.
+Here is what you are looking for. A python code to work the same way as the moses' `multi-bleu.perl`. What's more, it can also be used as a module of python. Just `import bleu` and use `bleu.corpus_bleu`, etc. to do your work in detail.
 
 ## Features
 * Can be used like moses BLEU
 * Perform well in python 2&3
 * Easily extend to BLEU-n score
 * Call it in python code without creating files
+* A fast realization of timestep-concerned sentence bleu
 
 ## How to Use
 * If there are files you want to test:
@@ -19,12 +20,15 @@ Here is what you are looking for. A python script to work the same as the moses'
     * This will print to screen the BLEU scores like `BLEU = 48.39, 76.9/56.0/42.1/32.0 (BP=0.986, ratio=0.986, hyp_len=19588, ref_len=19865)`. Just same as moses.
 * If you want to use in python code:
     * `import bleu`
-    * `bleu_count(hypothesis, references, max_n=4)` does the most important count work for BLEU. It returns `clip_count (correct), count (total), len_hyp, len_ref(sum of closest_length)`
-    * `corpus_bleu(hypothesis, references, max_n=4)` does the complete BLEU calculation. It returns `BLEU([final bleu, p_1, p_2, ..., p_max_n]), additional message([BP, ratio, hyp_len, ref_len]) `
-    * `hypothesis` should be a 1-dimension string list, and `references` should be a 2-dimension string list.
+    * If not specified, all the `hypothesis` should be a 1-dimension string list, and all the `references` should be a 2-dimension string list.
+    * `bleu.bleu_count(hypothesis, references, max_n=4)` does the most important count work for BLEU. It returns `clip_count (correct), count (total), len_hyp, len_ref(sum of closest_length)`
+    * `bleu.corpus_bleu(hypothesis, references, max_n=4)` does the complete BLEU calculation. It returns `BLEU([final bleu, p_1, p_2, ..., p_max_n]), additional message([BP, ratio, hyp_len, ref_len]) `
+    * `bleu.incremental_bleu_count(hypothesis, references, max_n=4)` does an incremental count for BLEU, which means it will return the correct count for every timestep(prefix) of every hypothesis.
+    * `bleu.incremental_sent_bleu(hypothesis, references, max_n=4)` does the bleu calculation work according to the result of `bleu.incremental_bleu_count`. And return all the bleu scores according to the timestep(prefix). **ATTENTION**: In this function, `hypothesis` should be a string and `references` should be a string list.
+    * `bleu.incremental_test_corpus_bleu(hypothesis, references, max_n=4)` is just a test function for `bleu.incremental_bleu_count`. However, it behaves the same way as `bleu.corpus_bleu`, except for more time(a little) and memories.
 
 ## Something Wrong?
-* Make sure your files are UTF-8 formatted.
+* Make sure your files are UTF-8 formatted. And you should convert all your strings to unicode before calling any of the above functions.
 * If there is any Error (Exception) Message, Read it.
 * Contact the author via Issues or E-mail.
 
--- bleu.py
+++ bleu.py
@@ -4,36 +4,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import argparse
-import os
 import math
-import codecs
-parser = argparse.ArgumentParser(
-    description="Calculate BLEU scores for the input hypothesis and reference files")
-parser.add_argument(
-    "-hyp",
-    nargs=1,
-    dest="pf_hypothesis",
-    type=str,
-    help="The path of the hypothesis file.")
-parser.add_argument(
-    "-ref",
-    nargs='+',
-    dest="pf_references",
-    type=str,
-    help="The path of the references files.")
-args = parser.parse_args()
-
-
-
+import copy
 
 def bleu_count(hypothesis, references, max_n=4):
     ret_len_hyp = 0
     ret_len_ref = 0
     ret_clip_count = [0]*max_n
     ret_count = [0]*max_n
-    for i in range(len(hypothesis)):
-        hyp, ref = hypothesis[i], references[i]
+    for m in range(len(hypothesis)):
+        hyp, ref = hypothesis[m], references[m]
         x = hyp.split()
         y = [r.split() for r in ref]
         x_len = len(x)
@@ -111,33 +91,146 @@ def corpus_bleu(hypothesis, references, max_n=4):
     bleu = brevity_penalty*math.exp(log_bleu / float(max_n))
     return [bleu]+bleu_scores, [brevity_penalty, total_len_hyp/total_len_ref, total_len_hyp, total_len_ref]
 
-def file_exist(pf):
-    if os.path.isfile(pf):
-        return True
-    return False
 
-if args.pf_hypothesis!=None or args.pf_references!=None:
-    if args.pf_hypothesis==None:
-        raise Exception("Missing references files...")
-    if args.pf_references==None:
-        raise Exception("Missing hypothesis files...")
+def incremental_bleu_count(hypothesis, references, max_n=4):
+    ret_len_hyp = []
+    ret_len_ref = []
+    ret_clip_count = []
+    ret_count = []
+    for m in range(len(hypothesis)):
+        hyp, ref = hypothesis[m], references[m]
+        x = hyp.split()
+        y = [r.split() for r in ref]
+        x_len = len(x)
+        y_len = [len(s) for s in y]
+        n_ref = len(ref)
 
-    n = None
-    data = []
-    for pf in args.pf_hypothesis+args.pf_references:
-        if not file_exist(pf):
-            raise Exception("File Not Found: %s"%(pf))
-        f = codecs.open(pf, encoding="utf-8")
-        data.append(f.readlines())
-        if n==None:
-            n = len(data[-1])
-        elif n != len(data[-1]):
-            raise Exception("Not parrallel: %s %d-%d"%(pf, n, len(data[-1])))
-        f.close()
+        ref_ngram = dict()
 
-    hyp_data = data[0]
-    ref_data = list(map(list, zip(*data[1:])))
+        for i in range(n_ref):
+            for n in range(max_n):
+                sent_ngram = dict()
+                for st in range(0, y_len[i]-n):
+                    ngram = "%d"%(n+1)
+                    for k in range(n+1):
+                        j = st+k
+                        ngram += " %s"%(y[i][j])
+                    if ngram not in sent_ngram:
+                        sent_ngram[ngram]=0
+                    sent_ngram[ngram]+=1
+                for ngram in sent_ngram.keys():
+                    if ngram not in ref_ngram or ref_ngram[ngram]<sent_ngram[ngram]:
+                        ref_ngram[ngram] = sent_ngram[ngram]
+        y_len = sorted(y_len)
+        ret_len_hyp.append([])
+        ret_len_ref.append([])
+        ret_clip_count.append([])
+        ret_count.append([])
 
-    bleu, addition = corpus_bleu(hyp_data, ref_data)
+        hyp_ngram = dict()
+        p_closest = 0
+        for i in range(x_len):
+            if i == 0:
+                ret_clip_count[-1].append([0]*max_n)
+                ret_count[-1].append([0]*max_n)
+            else:
+                ret_clip_count[-1].append(copy.deepcopy(ret_clip_count[-1][-1]))
+                ret_count[-1].append(copy.deepcopy(ret_count[-1][-1]))
 
-    print("BLEU = %.2f, %.1f/%.1f/%.1f/%.1f (BP=%.3f, ratio=%.3f, hyp_len=%d, ref_len=%d)"%(bleu[0]*100, bleu[1]*100, bleu[2]*100, bleu[3]*100, bleu[4]*100, addition[0], addition[1], addition[2], addition[3]))
+            j = i+1
+            ret_len_hyp[-1].append(i+1)
+            if j>y_len[p_closest]:
+                while j>y_len[p_closest] and p_closest<n_ref-1:
+                    p_closest+=1
+            tmp_closest_diff = 9999
+            tmp_closest_len = 9999
+            if p_closest>0 and (j-y_len[p_closest-1])<tmp_closest_diff:
+                tmp_closest_diff=j-y_len[p_closest-1]
+                tmp_closest_len = y_len[p_closest-1]
+            if p_closest<n_ref and (y_len[p_closest]-j)<tmp_closest_diff:
+                tmp_closest_diff=y_len[p_closest]-j
+                tmp_closest_len = y_len[p_closest]
+
+            ret_len_ref[-1].append(tmp_closest_len)
+            for n in range(max_n):
+                st = i-n
+                if st>=0:
+                    ngram = "%d"%(n+1)
+                    for k in range(n+1):
+                        j = st+k
+                        ngram += " %s"%(x[j])
+                    if ngram not in hyp_ngram:
+                        hyp_ngram[ngram]=0
+                    hyp_ngram[ngram]+=1
+                    ret_count[-1][-1][n] += 1
+                    if ngram in ref_ngram  and hyp_ngram[ngram]<=ref_ngram[ngram]:
+                        ret_clip_count[-1][-1][n] += 1
+
+    return ret_clip_count, ret_count, ret_len_hyp, ret_len_ref
+
+def incremental_sent_bleu(hypothesis, references, max_n=4):
+    clip_count, count, total_len_hyp, total_len_ref = incremental_bleu_count([hypothesis], [references], max_n=max_n)
+    clip_count = clip_count[0]
+    count = count[0]
+    total_len_hyp = total_len_hyp[0]
+    total_len_ref = total_len_ref[0]
+    n_len = len(clip_count)
+    ret = []
+    for i in range(n_len):
+        brevity_penalty = 1.0
+        bleu_scores = []
+        bleu = 0
+        for n in range(max_n):
+            if count[i][n]>0:
+                bleu_scores.append(clip_count[i][n]/count[i][n])
+            else:
+                bleu_scores.append(0)
+        if total_len_hyp[i] < total_len_ref[i]:
+            brevity_penalty = math.exp(1 - total_len_ref[i]/total_len_hyp[i])
+        def my_log(x):
+            if x == 0:
+                return -9999999999.0
+            elif x < 0:
+                raise Exception("Value Error")
+            return math.log(x)
+        log_bleu = 0.0
+        for n in range(max_n):
+            log_bleu += my_log(bleu_scores[n])
+        bleu = brevity_penalty*math.exp(log_bleu / float(max_n))
+        ret.append(bleu)
+    return ret
+
+def incremental_test_corpus_bleu(hypothesis, references, max_n=4):
+    assert(len(hypothesis) == len(references))
+    tmp_clip_count, tmp_count, tmp_total_len_hyp, tmp_total_len_ref = incremental_bleu_count(hypothesis, references, max_n=max_n)
+    clip_count = [0]*4
+    count = [0]*4
+    total_len_hyp = 0
+    total_len_ref = 0
+    for i in range(len(hypothesis)):
+        for n in range(4):
+            clip_count[n]+=tmp_clip_count[i][-1][n]
+            count[n] += tmp_count[i][-1][n]
+        total_len_hyp += tmp_total_len_hyp[i][-1]
+        total_len_ref += tmp_total_len_ref[i][-1]
+    brevity_penalty = 1.0
+    bleu_scores = []
+    bleu = 0
+    for n in range(max_n):
+        if count[n]>0:
+            bleu_scores.append(clip_count[n]/count[n])
+        else:
+            bleu_scores.append(0)
+    if total_len_hyp < total_len_ref:
+        brevity_penalty = math.exp(1 - total_len_ref/total_len_hyp)
+    def my_log(x):
+        if x == 0:
+            return -9999999999.0
+        elif x < 0:
+            raise Exception("Value Error")
+        return math.log(x)
+    log_bleu = 0.0
+    for n in range(max_n):
+        log_bleu += my_log(bleu_scores[n])
+    bleu = brevity_penalty*math.exp(log_bleu / float(max_n))
+    return [bleu]+bleu_scores, [brevity_penalty, total_len_hyp/total_len_ref, total_len_hyp, total_len_ref]
